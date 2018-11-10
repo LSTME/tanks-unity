c7d9e69933275444a238ddbe64378c49bbfc71a5
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2018-11-10 20:25:51 +0100

    Implemented HealArea

---
+++ Assets/Scripts/Areas/HealArea.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealArea : MonoBehaviour
+{
+    public float radius = 10.0f;
+    public LayerMask targetMask;
+
+    public float healingPower = 5.0f;
+
+    public ParticleSystem burstParticleSystem;
+
+    private void Awake()
+    {
+        StartCoroutine(Heal());
+    }
+
+    private IEnumerator Heal()
+    {
+        while (true)
+        {
+            var colliders = Physics.OverlapSphere(transform.position, radius, targetMask);
+            foreach (var targetCollider in colliders)
+            {
+                var tankHealth = targetCollider.GetComponent<TankHealth>();
+                tankHealth.Heal(healingPower);
+            }
+            
+            if (colliders.Length > 0)
+                burstParticleSystem.Emit(20);
+            
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}

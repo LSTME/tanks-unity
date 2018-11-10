5eec0892bc16407e7b5ff40ee47da98c2b660481
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2018-11-10 17:19:52 +0100

    Initial commit

---
+++ Assets/Scripts/UI/WeaponIndication.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponIndication : MonoBehaviour
+{
+    public GameManager gameManager;
+    public Text weaponText;
+
+    void Start()
+    {
+        weaponText.text = string.Empty;
+    }
+
+    void Update()
+    {
+        if (gameManager.RoundStatus != RoundStatus.Playing)
+        {
+            weaponText.text = string.Empty;
+            return;
+        }
+
+        weaponText.text = DateTime.Now.ToLongTimeString();
+    }
+}
